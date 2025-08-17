b016b5f9446a2a0bca18489bfed498e3744fdc9d
spyfaq <58468452+spyfaq@users.noreply.github.com>
2025-08-18 00:21:28 +0300

    correct online print

--- post_from_dropbox.py
+++ post_from_dropbox.py
@@ -14,7 +14,7 @@ DROPBOX_FOLDER = "/telegram_content"
 today_str = datetime.date.today().strftime("%Y-%m-%d")
 
 def list_dropbox_files():
-    print(DROPBOX_TOKEN)
+    print(DROPBOX_TOKEN, flush=True)
     url = "https://api.dropboxapi.com/2/files/list_folder"
     headers = {"Authorization": f"Bearer {DROPBOX_TOKEN}", "Content-Type": "application/json"}
     payload = {"path": DROPBOX_FOLDER}
